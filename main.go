82b92a2a1870951bc31a417103faf68ee126ba61
Kyle Terry <kyle@kyleterry.com>
2015-07-16 01:00:10 -0700

    finished quote server

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net"
 )
 
+var quotes []string
+
 func main() {
+	//make cache of quotes
+	input, err := ioutil.ReadFile("/etc/qotd/quotes.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := json.Unmarshal(input, &quotes); err != nil {
+		log.Fatal(err)
+	}
+
 	sock, err := net.Listen("tcp", "0.0.0.0:17")
 	if err != nil {
 		log.Fatal(err)
@@ -24,6 +38,7 @@ func main() {
 
 // Goroutine
 func handle(conn net.Conn) {
-	conn.Write([]byte("some quote"))
+	l := rand.Perm(len(quotes) - 1)
+	conn.Write([]byte(quotes[l[0]]))
 	conn.Close()
 }
